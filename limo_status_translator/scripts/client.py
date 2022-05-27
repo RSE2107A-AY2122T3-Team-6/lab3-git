7621a444575321f8b63f8e687eedc216d158a9aa
Dillon Chew <2101354@sit.singaporetech.edu.sg>
2022-05-27 19:41:16 +0800

    updated both client and server

--- limo_status_translator/scripts/client.py
+++ limo_status_translator/scripts/client.py
@@ -7,7 +7,31 @@ import random
 import rospy
 from limo_status_translator.srv import GetLimoStatus, GetLimoStatusRequest
 
+def talker(a,resp):
+        if a == 0:
+            pub = rospy.Publisher('limo_status/vehicle_state', String, queue_size = 10)
+            rate = rospy.Rate(1)
+            pub.publish(resp.status_string)
 
+        if a == 1:
+            pub = rospy.Publisher('limo_status/control_mode', String, queue_size = 10)
+            rate = rospy.Rate(1)
+            pub.publish(resp.status_string)
+
+        if a == 2:
+            pub = rospy.Publisher('limo_status/battery_voltage', String, queue_size = 10)
+            rate = rospy.Rate(1)
+            pub.publish(resp.status_string)
+
+        if a == 3:
+            pub = rospy.Publisher('limo_status/error_code', String, queue_size = 10)
+            rate = rospy.Rate(1)
+            pub.publish(resp.status_string)
+
+        if a == 4:
+            pub = rospy.Publisher('limo_status/motion_mode', String, queue_size = 10)
+            rate = rospy.Rate(1)
+            pub.publish(resp.status_string)
 
 if __name__ == "__main__":
     rospy.init_node('client')
@@ -27,6 +51,7 @@ if __name__ == "__main__":
         resp = service(req)
 
         rospy.loginfo("Recieved: " + resp.status_string)
-
+        talker(a,resp)
+        
         r.sleep()
     
